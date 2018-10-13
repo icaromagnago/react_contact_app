a38e89d832d43a325aac3923a3f766447c0d779b
Icaro Magnago <icaromagnago@gmail.com>
2018-10-12 21:19:31 -0300

    Adding React Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContact from './CreateContact';
+import { Route } from 'react-router-dom';
 
 
 class App extends Component {
@@ -31,7 +33,14 @@ class App extends Component {
 	render() {
 		return (
 		  <div>
-		    <ListContacts contacts={this.state.contacts} onDeleteContact={this.removeContact} />
+		  	<Route exact path='/' render={()	=> (
+				<ListContacts
+				    contacts={this.state.contacts}
+				    onDeleteContact={this.removeContact}
+			  	/>
+		  	)} />
+
+		  	<Route path='/create' component={CreateContact} />
 		  </div>
 		)
 	}
